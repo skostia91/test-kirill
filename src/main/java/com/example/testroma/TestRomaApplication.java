0c42716c2dda52ccfb8af48d8281f9c08d2240e9
Костя <@https://github.com/skostia91/git-test>
2023-11-27 23:18:03 +0300

    1.0

---
+++ src/main/java/com/example/testroma/TestRomaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testroma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testroma/TestRomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testroma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRomaApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
